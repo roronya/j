f239c07472ada4bdee8bbe8a9a5a6060d2ff6f6a
roronya <roronya628@gmail.com>
2021-09-30 15:55:15 +0900

    update

--- j/j.py
+++ j/j.py
@@ -1,21 +1,24 @@
 from jira import JIRA
 import os
 
+USER = os.environ['JIRA_USER']
+PASSWORD = os.environ['JIRA_PASSWORD']
+SERVER = os.environ['JIRA_SERVER']
 
-def main(project, epic, component, assignee, title, description):
-  user = os.environ['JIRA_USER']
-  password = os.environ['JIRA_PASSWORD']
-  server = os.environ['JIRA_SERVER']
-  options = {'server': server}
-  j = JIRA(options=options, basic_auth=(user, password))
-  new_issue = j.create_issue(
-      project=project,
-      summary=title,
-      description=description or "",
-      issuetype={'name': 'Task'},
-      priority={'id': '3'},
-      assignee={'name': assignee},
-      components=[{"name": component}],
-  )
-  j.add_issues_to_epic(epic, [new_issue.key])
-  return f"{server}/browse/{new_issue.key}"
+
+def main(project, summary, epic, component, assignee, description, issuetype, parent):
+    options = {'server': SERVER}
+    j = JIRA(options=options, basic_auth=(USER, PASSWORD))
+    issue_dict = {'project': project, 'summary': summary,
+                  'priority': {'id': '3'},
+                  'issuetype': {'name': issuetype if issuetype is not None else 'Task'}}
+    if description:
+        issue_dict['description'] = description
+    if assignee:
+        issue_dict['assignee'] = {'name': assignee}
+    if component:
+        issue_dict['components'] = [{"name": component}]
+    new_issue = j.create_issue(issue_dict)
+    if epic:
+        j.add_issues_to_epic(epic, new_issue.key)
+    return f"{SERVER}/browse/{new_issue.key}"
